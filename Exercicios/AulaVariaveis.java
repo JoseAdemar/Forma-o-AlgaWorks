b41dd7126ca1086071552af18ce34714ed58ab49
joseademar <joseademarti@gmail.com>
2024-01-01 21:47:04 -0300

    Primeiro commit, subindo exercícios já realizados.

---
+++ Exercicios/AulaVariaveis.java
@@ -0,0 +1,10 @@
+public class AulaVariaveis{
+
+  public static void main(String[] args){
+    int minhaIdade = 33;
+    int suaIdade = 30;
+    int totalIdades = minhaIdade + suaIdade;
+    System.out.println("Total das idades: " + totalIdades);
+  }
+}
+
