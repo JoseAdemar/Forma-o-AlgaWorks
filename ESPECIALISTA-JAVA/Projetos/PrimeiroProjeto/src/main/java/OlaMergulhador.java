acebd80f655454ceef8dcbe0eac62e1de1ec9a8d
joseademar <joseademarti@gmail.com>
2024-02-26 20:32:06 -0700

    Estudando a ferramenta Intellij idea

---
+++ ESPECIALISTA-JAVA/Projetos/PrimeiroProjeto/src/main/java/OlaMergulhador.java
@@ -0,0 +1,15 @@
+public class OlaMergulhador {
+
+    public static void main(String[] args) {
+        int idade = 33;
+        double altura = 1.75;
+
+        System.out.println(altura);
+
+        System.out.println("FIM!-!");
+        System.out.println("FIM!-!");
+        System.out.println("FIM!-!");
+        System.out.println("FIM!-!");
+        System.out.println("FIM!-!");
+    }
+}
