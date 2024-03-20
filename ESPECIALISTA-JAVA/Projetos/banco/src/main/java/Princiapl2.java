7e854445172b4a20703dae928c22cefdefb7f810
joseademar <joseademarti@gmail.com>
2024-03-20 18:11:22 -0300

    Estudando herança do Java

---
+++ ESPECIALISTA-JAVA/Projetos/banco/src/main/java/Princiapl2.java
@@ -0,0 +1,21 @@
+import com.aglaworks.banco.Conta;
+import com.aglaworks.banco.ContaInvestimento;
+import com.aglaworks.banco.Titular;
+
+public class Princiapl2 {
+    public static void main(String[] args) {
+        ContaInvestimento conta1 = new ContaInvestimento();
+        conta1.setTitular(new Titular("João da Silva", "12312312300"));
+        conta1.setAgencia(1234);
+        conta1.setNumero(999999);
+
+        conta1.imprimirDemonstrativo();
+
+        conta1.depositar(100);
+        conta1.imprimirDemonstrativo();
+
+        conta1.creditarRendimentos(6);
+        conta1.imprimirDemonstrativo();
+    }
+
+}
---
+++ ESPECIALISTA-JAVA/Projetos/estudando-record/src/main/java/com/algaworks/agenda/Principal.java
@@ -0,0 +1,16 @@
+package com.algaworks.agenda;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        Horario horario = new Horario(10, 30);
+
+        Agendamento agendamentoCabelo = new Agendamento(horario, "Corte de cabelo");
+        agendamentoCabelo.setHorario(new Horario(16, 20));
+
+
+        System.out.println(agendamentoCabelo.getHorarioFormatado());
+        System.out.println(horario.hora());
+    }
+
+}
