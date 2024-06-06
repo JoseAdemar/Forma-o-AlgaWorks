d29ee37acd9025709b593f4e5f34b962a08e56f9
joseademar <joseademarti@gmail.com>
2024-06-06 20:04:35 -0300

    Aprofundando os estudos de I/O do Java api antiga.

---
+++ ESPECIALISTA-JAVA/Projetos/io-antiga-java/src/main/java/aula05/Principal.java
@@ -0,0 +1,24 @@
+package aula05;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Instant;
+
+public class Principal {
+
+    public static void main(String[] args) throws IOException {
+        File pasta = new File("docs").getCanonicalFile();
+
+//        File[] arquivos = pasta.listFiles();
+//        File[] arquivos = pasta.listFiles(File::isFile);
+//        File[] arquivos = pasta.listFiles(File::isDirectory);
+        File[] arquivos = pasta.listFiles((dir, nome) -> nome.startsWith("c"));
+
+        if (arquivos != null) {
+            for (File arquivo : arquivos) {
+                System.out.println(arquivo.getName());
+            }
+        }
+    }
+
+}
